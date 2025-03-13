a9bfbe703ab75e5b03e9d84c5df69f96812bb842
theakish <111280313+Amin72012@users.noreply.github.com>
2025-03-13 20:02:49 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Initialize Supabase client with URL and Key
 const supabaseUrl = 'https://htjgdziushnvtpyigqrg.supabase.co';
-const supabaseKey = 'your-supabase-key-here'; // Ensure this key is correct
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0amdkeml1c2hudnRweWlncXJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4ODUxMzIsImV4cCI6MjA1NzQ2MTEzMn0.3CR3SFyprDOYGxEwAHV6XmYVbGY2oUq5rSC-XtlkV1g'; // Ensure this key is correct
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Signup function
